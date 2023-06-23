1bc0f2dab06861ed819eb69d637b48f09baf0e3c
Andre <ba.dijkman@icloud.com>
2023-06-23 21:32:17 +0200

    edit5

--- sell.py
+++ sell.py
@@ -19,62 +19,16 @@ def handleSell(parsed_Data):
 
     # Go through the inventory and get the product_name
     inStock = getAllItemsByNameFromInventoryCsv(name)
-    print(inStock)
 
     # Check how much of the item is in stock.
     inStockAmount = reduce(
         lambda x, y: x + y, [d["amount"] for d in inStock], 0)
-    print(f' er zijn {inStockAmount} {name} in stock')
 
     # Loop that handles the selling of the products.
 
-    # while amount > 0:
-    print(f'amount {amount}')
-    if inStock:
-        for stock in inStock:
-            print("in stock ")
-            print(stock['amount'])
+    print(f' parsed aantal om verkocht te worden {amount}')
+    print(f' er zijn {inStockAmount} {name} in stock')
+    print(inStock)
+    print (if instock: )
 
-            # the amount is higher than what is in stock
-            if amount > stock["amount"] and inStockAmount != 0:
-                print("ja")
-                break
-            elif inStockAmount == 0:
-                print(f"You were only able to buy {sold}")
-                amount = 0
-                break
-            else:
-                print("pass")
-                pass
-    #     if amount == stock["amount"]:
-    #         print(colored('OK', 'green',
-    #               attrs=["reverse", 'bold']))
-    # amount -= stock["amount"]
-    # inStockAmount -= stock["amount"]
-    # sold += stock["amount"]
-
-    # remove the line from the inventory.csv and write the into sell.csv
-    # appendRowToSoldCsv(
-    #     stock["id"], name, stock["amount"], day, price)
-    # removeLineFromInventoryCsv(int(stock["id"]))
-    # continue
-
-    # elif inStockAmount == 0:
-    #     print(f"You were only able to buy {sold}")
-    #     amount = 0
-    #     break
-    # else:
-    #     appendRowToSoldCsv(stock["id"], name, amount, day, price)
-    #     if amount == stock["amount"]:
-    #         removeLineFromInventoryCsv(int(stock["id"]))
-    #     else:
-    #         adjustLineInInventoryCsv(int(stock["id"]), amount)
-    #     # Set amount to 0 to reset the loop as the purchase has been fulfilled
-    #     amount = 0
-    #     sold += amount
-    #     print(colored('OK', 'green',
-    #                   attrs=["reverse", 'bold']))
-
-    else:
-        print(colored('ERROR: Product not in stock.', 'red'))
-        #  break
+   
