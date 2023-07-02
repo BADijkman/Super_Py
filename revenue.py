62dfdd224ad09569f009faa4564172cc1c7e32ae
Andre <ba.dijkman@icloud.com>
2023-07-02 21:57:16 +0200

    formatting

--- revenue.py
+++ revenue.py
@@ -1,8 +1,5 @@
-# import csv
-from console import console, err_console
-# from rich.table import Table
+from console import console
 from rich.align import Align
-# from utils.utils import getItemFromBoughtCsvById
 from datetime import datetime
 from modify_day.getDateFromFile import getDateFromFile
 from utils.utils import getAllItemsSoldByDate
@@ -10,7 +7,6 @@ from date import get_date
 
 
 def displayRevenue(parsed_data):
-
     day = getDateFromFile("str")
     totalRevenue = getAllItemsSoldByDate(day, parsed_data)
     revenueLine = f"  Total revenue : \u20ac {totalRevenue:.2f}"
@@ -21,7 +17,6 @@ def displayRevenue(parsed_data):
         console.print()
     elif parsed_data.startingdate is not None:
         display_date_from = getDateFromFile("str")
-
         display_date_to = get_date()
         console.rule(
             f"[yellow]Revenue: from {display_date_from} to {display_date_to}", style="yellow")
