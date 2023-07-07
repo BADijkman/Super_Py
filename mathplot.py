1f11776c8fa2ee099685c445d2fad479f013c496
Andre <ba.dijkman@icloud.com>
2023-07-07 14:13:45 +0200

    add mathplot

--- mathplot.py
+++ mathplot.py
@@ -2,30 +2,51 @@ import matplotlib.pyplot as plt
 from utils.utils import inStocktotal, inStockTotalNotExpired
 
 
+def checkForDuplicateProducts(new_list):
+    updated_list = []
+    for dict in new_list:
+        for key, value in dict.items():
+            if key == "name":
+                search_value = value
+            elif key == "amount":
+                input_Amount = value
+
+        new_dict = True
+    # Check if the search_value exists in any of the dictionaries and       modify amount
+        for dict in updated_list:
+            if search_value in dict.values():
+                amount = dict.get("amount")
+                new_Amount = amount + input_Amount
+                dict["amount"] = new_Amount
+                new_dict = False
+                break
+
+    # If the search_value is not found, create a new dictionary
+        if new_dict:
+            new_dict = {"name": search_value, "amount": input_Amount}
+            updated_list.append(new_dict)
+
+    return (updated_list)
+
+
 def pltShow():
     # get Inventory
     instock = inStocktotal()
     inStockTotal = inStockTotalNotExpired(instock)
-    print(inStockTotal)
+    # print(inStockTotal)
+    listWithoutDuplicateProducts = checkForDuplicateProducts(inStockTotal
+                                                             )
+
+    # products = [d['name'] for d in listWithoutDuplicateProducts]
+    # amounts = [d['amount'] for d in listWithoutDuplicateProducts]
 
     fig, ax = plt.subplots()
 
-    # newList = []
-    newDict = {}
-    for dict in inStockTotal:
-        
-        
-   
-
-    # newDict=
-    # newList.append(newDict)
-
-    # print(inStockSorted)
-    fruits = ["apple", "pear"]
-    counts = [5, 5]
+    products = [d['name'] for d in listWithoutDuplicateProducts]
+    amounts = [d['amount'] for d in listWithoutDuplicateProducts]
     bar_colors = ['tab:green', 'tab:blue']
 
-    ax.bar(fruits, counts,  color=bar_colors)
+    ax.bar(products, amounts,  color=bar_colors)
 
     ax.set_title('Inventory')
     ax.set_ylabel('amount')
