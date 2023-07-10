85516fb1b14025812ba942c54d18765ee51cf367
Andre <ba.dijkman@icloud.com>
2023-07-10 17:56:50 +0200

    formatting

--- inventory.py
+++ inventory.py
@@ -7,9 +7,8 @@ from matplot import pltShow
 from handle_date import Date
 from handle_cvs import Inventory
 
+
 # displayInventory
-
-
 def displayInventory(parsed_Data):
     table = Table(min_width=90, style='white',
                   header_style="green",
